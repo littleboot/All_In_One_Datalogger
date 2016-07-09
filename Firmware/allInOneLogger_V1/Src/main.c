4bfdf10b1333ba494b064b1ef9cd0a56a1149703
Tom Klijn <tomklijn@outlook.com>
2016-07-10 00:52:51 +0200

    added humidity check
    also added some commentary

--- Firmware/allInOneLogger_V1/Src/main.c
+++ Firmware/allInOneLogger_V1/Src/main.c
@@ -37,6 +37,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 
 /* USER CODE END Includes */
@@ -56,9 +57,10 @@ UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-RTC_DateTypeDef date;
-RTC_TimeTypeDef time;
+RTC_DateTypeDef date; //stores date to be configured
+RTC_TimeTypeDef time; //stores time to be configured
 
+bool CO2SensorReady = false; //false if still warming up (3min), if ready changed to true
 
 /* USER CODE END PV */
 
@@ -117,12 +119,9 @@ void update_display_sensordata(void);
 /* USER CODE BEGIN 0 */
 /**
 	* TO DO:
-	* -RTC configure time once. and use battery to continue RTC when powered off
-	* -Add filled background image nextion display use progress bar to manipulate image (crop) and show icons filling based on there values
-	* -Check bounds humidity sensor (check datsheet) bound between 0 and 100
 	* -Save and recall variables from EEPROM Like thingsspeak channel key
 	* -Add fucntion that gets temp from previous humidity measurement to speed up the code
-	* -After power on the CO2 sensor shows wrong values. show dots the first 2 minutes at CO2 level istead of wrong values
+	* -After power on the CO2 sensor shows wrong values. show dots the first 3 minutes at CO2 level istead of wrong values, 
 	* 
 	* TO DO Later:
 	* -Add option to configure dark and light voltage levels LDR
@@ -482,7 +481,12 @@ int get_humidity(void)
 	HAL_I2C_Master_Transmit(&hi2c1, (0x40<<1),&cmd, 1, 0xFF); //this function takes a byte as input adress. because the adress in the datasheet is 0x40 (7-Bit) it needs to be shifted left by one
 	HAL_I2C_Master_Receive(&hi2c1, (0x40<<1), I2CDataReceived, 2, 0xFF); //Note timeout is higher then the get_temp function
 	uint16_t RHCode = (uint16_t)I2CDataReceived[0]<<8 | (uint16_t)I2CDataReceived[1]; //convert the received MSB en LSB to one 16-bit word.
-	return ((125*RHCode)/65536)-6;
+	
+	//make sure value is between 0 and 100%, recommended in datasheet sensor
+	int humidity = ((125*RHCode)/65536)-6;
+	if(humidity > 100) humidity = 100;
+	if(humidity < 0 ) humidity = 0;
+	return humidity;
 }
 
 int get_CO2(void)
